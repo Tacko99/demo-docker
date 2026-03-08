eaf5eb2d29c4828fde9d8b7387b5814f55131668
Tacko99 <105995801+Tacko99@users.noreply.github.com>
2026-03-08 23:37:51 +0000

    Update User.java

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -6,7 +6,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
-public static class User {
+public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,7 +22,7 @@ public static class User {
         this.email = email;
     }
 
-    // Getters et setters
+    // getters et setters
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
     public String getName() { return name; }
