54eb1f83c3673bc867f78038a6ba6780990ed7e5
Tacko99 <105995801+Tacko99@users.noreply.github.com>
2026-03-08 23:39:19 +0000

    Update DataInitializer.java

--- src/main/java/com/example/demo/DataInitializer.java
+++ src/main/java/com/example/demo/DataInitializer.java
@@ -6,7 +6,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
-public static class DataInitializer implements CommandLineRunner {
+public class DataInitializer implements CommandLineRunner {
 
     private final UserRepository userRepository;
 
