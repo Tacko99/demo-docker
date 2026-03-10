59cff3336368287201e9269e28dc10b656a74563
Tacko99 <105995801+Tacko99@users.noreply.github.com>
2026-03-10 13:06:52 +0000

    Update User.java

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -4,8 +4,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+// Échapper le nom "user" pour H2
+@Table(name = "\"user\"")
 public class User {
 
     @Id
